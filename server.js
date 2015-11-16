ad4fd94872fc0f6a44d2d111f718e381db8291b8
George <7jagjag@gmail.com>
2015-11-16 12:18:58 -0600

    exercise 3

--- server.js
+++ server.js
@@ -4,27 +4,28 @@
 
 const Hapi = require('hapi');
 const server = new Hapi.Server();
+const Inert = require('inert');
+const Path = require('path')
 
 server.connection({ 
     host: 'localhost', 
     port: Number(process.argv[2] || 8080) 
 });
 
-server.route({
-    method: 'GET',
-    path:'/',
-    handler: function (request, reply) {
-        return reply("Hello hapi")
-    }
-});
+
+
+server.register(Inert, err=>{
+    if(err) throw err;
+})
+
 
 server.route({
     method: 'GET',
-    path: '/{name}',
-    handler: (req , reply) => {
-        return reply(`Hello ${req.params.name}`)
+    path:'/',
+    handler: {
+        file: 'index.html'
     }
-})
+});
 
 server.start(err => {
     if (err) {
