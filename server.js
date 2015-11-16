2323ae7832d64e3fa8a7fe29baa316cfc8a405c4
George <7jagjag@gmail.com>
2015-11-16 11:59:42 -0600

    exercise 1 done

---
+++ server.js
@@ -0,0 +1,26 @@
+/**
+ * Makemehapi tutorial
+ */
+
+const Hapi = require('hapi');
+const server = new Hapi.Server();
+
+server.connection({ 
+    host: 'localhost', 
+    port: Number(process.argv[2] || 8080) 
+});
+
+server.route({
+    method: 'GET',
+    path:'/',
+    handler: function (request, reply) {
+        return reply("Hello hapi")
+    }
+});
+
+server.start(err => {
+    if (err) {
+        throw err;
+    }
+    console.log('Server running at:', server.info.uri);
+});
