5a2477c461285f8762dffaf86864ade7817ea673
George <7jagjag@gmail.com>
2015-11-16 21:02:09 -0600

    exercise 7

--- server.js
+++ server.js
@@ -4,47 +4,38 @@
 
 const Hapi = require('hapi');
 const server = new Hapi.Server();
-const H2o2 = require('h2o2');
-const Inert = require('inert')
+const Inert = require('inert');
+const Path = require('path')
+const Vision = require('vision');
 
 server.connection({ 
     host: 'localhost', 
     port: Number(process.argv[2] || 8080) 
 });
 
-server.register(Inert, err => {
-    if(err) throw err
+
+server.register(Vision, err => {
+    if(err) throw err;
+})
+server.register(Inert, err=>{
+    if(err) throw err;
 })
 
-server.register(H2o2, err => {
-    if(err) throw err
-});
+server.views({
+    engines: {
+        html: require('handlebars')
+    },
+    path: Path.join(__dirname, 'templates'),
+    helpersPath: 'helpers'
+})
 
 server.route({
     method: 'GET',
     path:'/',
     handler: {
-        file: 'public/file.html'
+        view: 'index.html'
     }
-})
-
-server.route({
-    method:'GET',
-    path: '/proxy',
-    handler: {
-        proxy: {
-            mapUri: (req, cb) => {
-                console.log('doing some aditional stuff before redirecting');
-                console.log(req.query)
-                cb(null, req.query.name || "https://github.com")
-            },
-            onResponse: (err, res, req, reply, settings, ttl) => {
-                console.log('receiving the response from the upstream.');
-                reply(res).header('X-Frame-Options', 'ALLOW-FROM http://localhost:8080/')
-            }
-        }
-    }
-})
+});
 
 server.start(err => {
     if (err) throw err;
